40b3ac3a3adb5f5a87187da73baf127a398637cc
Welinton Faria <welintonfm@gmail.com>
2021-07-11 10:43:50 -0300

    adcionando API getPokemon, que retorna os dados tratados do pokemon para o front

---
+++ pages/api/getPokemon.js
@@ -0,0 +1,39 @@
+import Axios from "axios"
+
+export default (req, res) => {
+  let pokemon_id = req.query.pokemon_id
+  Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon_id}/`).then((response) => {
+    let pokemon = response.data 
+    let types= pokemon.types.map(({ type }) => type.name)
+    let abilities= pokemon.abilities.map(({ability}) => ability)
+    let stats = new Object;
+
+    pokemon.stats.forEach(type => {
+      stats[type.stat.name.replace("-", "_")] = type.base_stat
+    });
+
+    console.log(stats)
+    console.log(stats.attack)
+
+    res.status(200).json({
+       name: pokemon.name,
+       artwork: pokemon.sprites.other['official-artwork'].front_default,
+       weight: pokemon.weight,
+       height: pokemon.height,
+       types: types,
+       abilities: abilities,
+       speed: stats.speed,
+       attack: stats.attack,
+       sp_attack: stats.special_attack,
+       defense: stats.defense,
+       sp_defense: stats.special_defense,
+       hp: stats.hp,
+      })
+  }).catch(err => {
+    console.log(err)
+    res.status(err.response.status).json({
+      message: err.message
+    })
+  })
+}
+  
