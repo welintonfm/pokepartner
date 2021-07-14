a2da77186f974ae4175cf021e8874f4c6ee44d52
Welinton Faria <welintonfm@gmail.com>
2021-07-13 23:42:15 -0300

    mostrando habilidades

--- components/pokemonAbilities/PokemonAbilitiesList.js
+++ components/pokemonAbilities/PokemonAbilitiesList.js
@@ -4,13 +4,16 @@ import PokemonAbilityTextBox from './PokemonAbilityTextBox';
 
 const PokemonAbilitiesList = (props) => {
     const [ability, setAbility] = useState()
+
+    useEffect(() => console.log(ability));
+
     return(
         <div className="abilities-list">
             <h2>Abilities</h2>
             <div>
             <ul>
                 {props.pokemon.abilities.map((ability, index)=> {
-                        return <PokemonAbilityButton key={index} ability={ability.name} url={ability.url}/>
+                        return <PokemonAbilityButton getAbility={setAbility} key={index} ability={ability.name} url={ability.url}/>
                         }
                     )}
                 </ul>
--- components/pokemonAbilities/PokemonAbilityButton.js
+++ components/pokemonAbilities/PokemonAbilityButton.js
@@ -1,10 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import Axios from 'axios'
+
+async function getAbility(url, callback){
+    await Axios.get(url).then((response) => {
+        let ability = response.data.effect_entries.map(entry => {if(entry.language.name == "en") return entry.short_effect})
+        ability = ability.filter(function (ability) {
+            return ability != null;
+          })[0]
+        callback(ability)
+        console.log(ability)
+        return ability
+    }).catch(err => {
+        console.error(err)
+        return
+    })
+}
 
 const PokemonAbilityButton = (props) => {
 
     return(
         <li>
-            <button className="ability-button">{props.ability}</button>
+            <button className="ability-button" onClick={async () => getAbility(props.url, props.getAbility)}>{props.ability}</button>
         </li>
     )
 }
